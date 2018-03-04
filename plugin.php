16bb9ad7b2f4043e64d5bff9dee6addfcf07f273
kyro46 <cjobst@wifa.uni-leipzig.de>
2018-03-04 15:22:28 +0100

    Set ilias_max_version to 5.3.x

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@ $version = "1.1.13";
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.2.0";
-$ilias_max_version = "5.3.0";
+$ilias_max_version = "5.3.999";
 
 // add the responsible person for the plugin
 
